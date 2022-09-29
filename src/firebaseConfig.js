47e9966b6f7ea25e43fb55bcebed8acdb3adb971
Axel Fuzzi <axelfuzziii@gmail.com>
2022-09-29 12:03:03 -0300

    variables_de_entorno

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDkcZ2BJX_CxAT3iH0Kj0b_Bqi924VGgbM",
-  authDomain: "reacttiendahardware.firebaseapp.com",
-  projectId: "reacttiendahardware",
-  storageBucket: "reacttiendahardware.appspot.com",
-  messagingSenderId: "707251883498",
-  appId: "1:707251883498:web:65677eefd00f8baefc3693"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
